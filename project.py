f43c6a2c72f2116f46ae907cce4a5ce39b716653
athenova <aleksey.sergeyev@yandex.com>
2025-03-16 11:01:11 +0300

    Posters

--- project.py
+++ project.py
@@ -3,6 +3,9 @@ from simple_blogger import CommonBlogger
 from simple_blogger.generators.YandexGenerator import YandexImageGenerator
 from datetime import datetime
 from datetime import timedelta
+from simple_blogger.senders.TelegramSender import TelegramSender
+#from simple_blogger.senders.InstagramSender import InstagramSender
+
 
 class Project(CommonBlogger):
     def _example_task_creator(self):
@@ -34,13 +37,13 @@ class Project(CommonBlogger):
 
     def __init__(self, **kwargs):
         super().__init__(
-            review_chat_id=-1002374309134,
-            production_chat_id='@coffee_and_nerves',
             first_post_date=datetime(2025, 3, 9),
             #days_to_review=timedelta(2),
             days_between_posts=timedelta(7),
             image_generator=YandexImageGenerator(),
             #text_generator=OpenAITextGenerator(),
+            reviewer=TelegramSender(),
+            senders=[TelegramSender(channel_id=f"@coffee_and_nerves")],
             topic_word_limit=100,
             **kwargs
         )
